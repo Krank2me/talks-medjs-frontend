5ac02635108b98740c865e06911380c5178de2f8
Frank Betancur <alejok6@gmail.com>
2022-05-09 23:01:32 -0500

    feat: add toast, level talk and company name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Banner } from './components/banner/Banner'
 import { TalkForm } from './components/talkForm/TalkForm'
+import { Toaster } from "react-hot-toast";
 
 
 
 export const App = () => {
   return (
     <Fragment>
+      <Toaster position="top-right" reverseOrder={false} />
       <Banner />
       <TalkForm />
     </Fragment>
--- src/components/talkForm/TalkForm.js
+++ src/components/talkForm/TalkForm.js
@@ -1,29 +1,47 @@
 import React, { useState } from "react";
 import { addTalk } from "../../services/AddTalkService";
+import toast from "react-hot-toast";
 import "./talkForm.css";
 
 export const TalkForm = () => {
   const initialStateValues = {
-    fullName: '',
-    email: '',
-    userTwitter: '',
-    talkTitle: '',
-    talkDescription: '',
-    speakerDescription: '',
+    fullName: "",
+    email: "",
+    userTwitter: "",
+    userCompany: "",
+    talkTitle: "",
+    talkLevel: "",
+    talkDescription: "",
+    speakerDescription: "",
   };
   const [values, setValues] = useState(initialStateValues);
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setValues({...values, [name]: value});
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('values: ', values);
-    const response =  await addTalk(values);
-    console.log("🚀 ~ file: TalkForm.js ~ line 24 ~ handleSubmit ~ response", response);
-    setValues({...initialStateValues});
+    console.log("values: ", values);
+    const response = await addTalk(values);
+    console.log(
+      "🚀 ~ file: TalkForm.js ~ line 24 ~ handleSubmit ~ response",
+      response
+    );
+    if (response.status === 200) {
+      console.log("response.status: ", response.status);
+      toast.success(
+        "Charla registrada éxitosamente",
+        {duration: 3000}
+      );
+      setValues({ ...initialStateValues });
+    } else {
+      toast.error(
+        "Ha ocurrido un error, por favor contactanos por medio de las redes de @medellinjs",
+        {duration: 3000}
+      );
+    }
   };
 
   return (
@@ -52,13 +70,21 @@ export const TalkForm = () => {
           onChange={handleInputChange}
           value={values.email}
         />
-        <label className="talk-container__form--label">Usuario Twitter</label>
+        <label className="talk-container__form--label">Usuario Twitter (ej: @jhondoe)</label>
         <input
           type="text"
           className="talk-container__form--input"
           name="userTwitter"
           onChange={handleInputChange}
           value={values.userTwitter}
+        />
+        <label className="talk-container__form--label">Empresa</label>
+        <input
+          type="text"
+          className="talk-container__form--input"
+          name="userCompany"
+          onChange={handleInputChange}
+          value={values.userCompany}
         />
         <label className="talk-container__form--label">
           Título de la charla
@@ -70,6 +96,24 @@ export const TalkForm = () => {
           onChange={handleInputChange}
           value={values.talkTitle}
         />
+        <label className="talk-container__form--label">
+          Nivel de la charla
+        </label>
+        <select
+          name="talkLevel"
+          onChange={handleInputChange}
+          className="talk-container__form--input"
+        >
+          <option key={1} value="principiante">
+            Principiante
+          </option>
+          <option key={2} value="intermedio">
+            Intermedio
+          </option>
+          <option key={3} value="avanzado">
+            Avanzado
+          </option>
+        </select>
         <label className="talk-container__form--label">
           Descripción corta de la charla
         </label>
@@ -84,8 +128,9 @@ export const TalkForm = () => {
         <label className="talk-container__form--label">
           Breve descripción sobre usted (trayectoria, ocupación, etc)
         </label>
-        <input
-          type="text"
+        <textarea
+          rows="50"
+          cols="50"
           className="talk-container__form--textarea"
           name="speakerDescription"
           onChange={handleInputChange}
